85ee64c04329155b7868fe11117baf8efc186dd3
haryu <hryau6@gmail.com>
2021-12-10 11:13:35 +0900

    conf: management session store by redis

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,6 +71,8 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.session_store :redis_store, { servers: 'redis://localhost:6379', expire_after: 1.day }
+
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
