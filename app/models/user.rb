2a4cf35ab9ba4b16a50f0b47d9ecbe5936395e33
haryu <hryau6@gmail.com>
2021-12-10 12:55:57 +0900

    add: basic css

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,8 @@ class User < ApplicationRecord
   validates :username, uniqueness: true, presence: true
 
   has_many :posts, dependent: :destroy
+
+  def own?(object)
+    id == object.user_id
+  end
 end
