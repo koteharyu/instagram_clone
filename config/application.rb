e3597e3656baee24daefe1925b22ac89ee6c8e9f
haryu <hryau6@gmail.com>
2021-12-10 10:56:39 +0900

    conf: time zone

--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,15 @@ module InstagramClone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+
     config.generators do |g|
       g.skip_routes true
       g.assets false
       g.helper false
     end
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
     # Configuration for the application, engines, and railties goes here.
     #
