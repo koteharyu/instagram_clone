77d1071a48a7af21cd1c766f420064dcd8ee5328
haryu <hryau6@gmail.com>
2021-12-10 11:33:10 +0900

    add: basic users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to root_path, success: 'create a user'
+    else
+      flash.now[:danger] = 'mistake'
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :password, :password_confirmation)
+  end
+end
