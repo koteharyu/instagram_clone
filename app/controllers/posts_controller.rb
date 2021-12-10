a7b8ecb432b1da6909ed7923d09ecce1b17ee6ed
haryu <hryau6@gmail.com>
2021-12-10 14:20:48 +0900

    add: for N+1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :require_login, only: %i[show new create edit update destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.includes(:user)
   end
 
   def show
