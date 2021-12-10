3a52d3e4a202e6a09461d12ed0cda669f4378e1e
haryu <hryau6@gmail.com>
2021-12-10 14:36:55 +0900

    add: order to posts/index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :require_login, only: %i[show new create edit update destroy]
 
   def index
-    @posts = Post.all.includes(:user)
+    @posts = Post.all.includes(:user).order(created_at: :desc)
   end
 
   def show
