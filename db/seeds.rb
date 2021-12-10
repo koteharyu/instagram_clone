f6925f769c5a4275c693a070da638601a8a44e8d
haryu <hryau6@gmail.com>
2021-12-10 15:10:21 +0900

    fix: for attention

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 10.times do
-  user = User.create(email: Faker::Internet.unique.email, username: Faker::Internet.user_name, password: 'password', password_confirmation: 'password')
+  User.create(email: Faker::Internet.unique.email,
+                     username: Faker::Internet.user_name,
+                     password: 'password',
+                     password_confirmation: 'password')
 end
 
 User.limit(10).each do |user|
-  user.posts.create(body: "im #{user.username}", images: [open("#{Rails.root}/db/fixtures/dummy1.jpeg"), open("#{Rails.root}/db/fixtures/dummy2.jpeg")])
+  user.posts.create(body: "im #{user.username}",
+                    images: [open("#{Rails.root}/db/fixtures/dummy1.jpeg"),
+                             open("#{Rails.root}/db/fixtures/dummy2.jpeg")
+                            ]
+                    )
 end
