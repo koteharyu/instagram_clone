51598740fd5a36874de778ce2a7c1b7906e152fb
haryu <hryau6@gmail.com>
2021-12-11 11:57:42 +0900

    add: avatar column to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 # Table name: users
 #
 #  id               :bigint           not null, primary key
+#  avatar           :json
 #  crypted_password :string
 #  email            :string           not null
 #  salt             :string
