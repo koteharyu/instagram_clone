25e1f0e69c030d4069824e1ab52c56c8a17fc6e3
haryu <hryau6@gmail.com>
2021-12-13 11:55:11 +0900

    add: system spec of posts

--- spec/support/login_helper.rb
+++ spec/support/login_helper.rb
@@ -6,4 +6,11 @@ module LoginHelper
     fill_in 'パスワード', with: 'password'
     click_button 'ログイン'
   end
+
+  def login_as
+    visit login_path
+    fill_in 'メールアドレス', with: user.email
+    fill_in 'パスワード', with: 'password'
+    click_button 'ログイン'
+  end
 end
