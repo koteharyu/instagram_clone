bce0791e31e65869384224d54556bd4cbe462ed9
haryu <hryau6@gmail.com>
2021-12-13 10:36:28 +0900

    add: system spec of auth

---
+++ spec/support/login_helper.rb
@@ -0,0 +1,9 @@
+module LoginHelper
+  def login
+    user = create(:user)
+    visit login_path
+    fill_in 'メールアドレス', with: user.email
+    fill_in 'パスワード', with: 'password'
+    click_button 'ログイン'
+  end
+end
